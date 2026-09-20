129862f9c69bcbae59ab976805ff2de7f5e6d0ed
SANJEEV KUMAR <kumarsanjeev01254@gmail.com>
2026-09-20 10:40:39 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./componets/About/About"
 import Home from "./componets/Home/Home"
 import Nav from "./componets/Nav/Nav"
 import Project from "./componets/Projects/Project"
-import Contact from "./componets/Contact/Contect"
+import Contact from "./componets/Contact/Contact"
 import Footer from "./componets/Footer/Footer"
 
 
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-<Nav />
+<Nav /> 
 <Home />
 <About />
 <Project />
